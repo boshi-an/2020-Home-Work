7d6ff6fc8b5157f8ef1cfd9da3c2a23f845447a5
totorato <41010290+totorato@users.noreply.github.com>
2020-02-16 17:59:36 +0800

    2.16

--- Complexity(AGC033D)/main.cpp
+++ Complexity(AGC033D)/main.cpp
@@ -2,7 +2,41 @@
 
 using namespace std;
 
+const int MX = 186;
+
+int n, m;
+char mp[MX][MX];
+int f[MX][MX][MX];
+int sum0[MX][MX], sum1[MX][MX];
+
+void input()
+{
+	scanf("%d%d", &n, &m);
+	for(int i=1; i<=n; i++) scanf("%s", mp[i]+1);
+}
+
+void work()
+{
+	for(int i=1; i<=n; i++)
+		for(int j=1; j<=m; j++)
+		{
+			sum0[i][j] = sum0[i][j-1] + (mp[i][j]=='.');
+			sum0[i][j] = sum0[i][j-1] + (mp[i][j]=='#');
+		}
+	for(int i=1; i<=n; i++)
+	{
+		for(int j=1; j<=m; j++)
+		{
+			for(int k=j; k<=m; k++)
+			{
+			}
+		}
+	}
+}
+
 int main()
 {
+	input();
+	work();
 	return 0;
 }
