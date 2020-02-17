8c4b4f1fc7028ff7ef1d6b040f3b1c331ead5d52
totorato <41010290+totorato@users.noreply.github.com>
2020-02-17 12:03:57 +0800

    2.17

--- Incenters(AGC039D)/main.cpp
+++ Incenters(AGC039D)/main.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
+typedef complex<double> Z;
+
+const int MX = 6005;
+const double pi = acos(-1);
+
+int n, len;
+int pos[MX];
+
 int main()
 {
+	scanf("%d%d", &n, &len);
+	for(int i=1; i<=n; i++) scanf("%d", &pos[i]), pos[n+i] = pos[i]+len;
+	Z sum(0, 0);
+	for(int i=n+1; i<=n*2; i++)
+	{
+		Z s_ang(0, 0);
+		for(int j=i-1; j>i-n; j--)
+		{
+			Z mid = exp(Z(0, pi*(pos[i]+pos[j])/len));
+			Z lft = exp(Z(0, 2*pi*pos[i]/len));
+			Z rgt = exp(Z(0, 2*pi*pos[j]/len));
+			sum += mid * Z(i-j-1, 0) + (lft-mid) * s_ang;
+			s_ang += exp(Z(0, pi*(pos[j]-pos[i])/len));
+		}
+	}
+	sum /= (1.0*n*(n-1)*(n-2) / 6);
+	printf("%.12lf %.12lf\n", -sum.real(), -sum.imag());
 	return 0;
 }
