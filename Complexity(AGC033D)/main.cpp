ac5a69a9b10e9f801bec4235724b7f296febb981
totorato <41010290+totorato@users.noreply.github.com>
2020-02-17 09:37:54 +0800

    2.17

--- Complexity(AGC033D)/main.cpp
+++ Complexity(AGC033D)/main.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int MX = 186;
+const int MX = 190;
 
 int n, m;
 char mp[MX][MX];
@@ -28,38 +28,53 @@ void work()
 			sum[0][i][j] = sum[0][i][j-1] + (mp[i][j]=='.');
 			sum[1][i][j] = sum[1][i][j-1] + (mp[i][j]=='#');
 		}
-	for(int i=1; i<=n; i++)
-	{
+	for(int i=n; i>=1; i--)
 		for(int j=1; j<=m; j++)
-		{
 			for(int k=j; k<=m; k++)
-			{
 				for(int c=0; c<2; c++)
-				{
 					if(sum[c][i][k]-sum[c][i][j-1]==k-j+1)
 					{
-						if(sum[c][i+1][k]-sum[c][i+1][j-1]==k-j+1) f[i][j][k] = f[i+1][j][k]+1;
-						else f[i][j][k] = 1;
+						if(sum[c][i+1][k]-sum[c][i+1][j-1]==k-j+1) cmax(f[i][j][k], f[i+1][j][k]+1);
+						else cmax(f[i][j][k], 1);
 					}
-					else f[i][j][k] = 0;
-				}
-			}
-		}
+	if(f[1][1][m] == n)
+	{
+		printf("%d\n", 0);
+		return;
 	}
-	for(int r=1; r<=14; r++)
+	for(int r=1; r<=15; r++)
 	{
 		for(int i=1; i<=n; i++)
-		{
 			for(int j=1; j<=m; j++)
-			{
-				for(int k=j+1; k<=m; k++)
-				{
+				for(int k=j; k<=m; k++)
+					cmax(g[j][i][i+f[i][j][k]-1], k-j+1);
+		for(int i=1; i<=n; i++)
+			for(int j=1; j<=m; j++)
+				for(int k=m; k>=j; k--)
+					cmax(f[i][j][k], f[i][j][k+1]);
+		for(int i=1; i<=m; i++)
+			for(int j=1; j<=n; j++)
+				for(int k=n; k>=j; k--)
+					cmax(g[i][j][k], g[i][j][k+1]);
+		for(int i=1; i<=n; i++)
+			for(int j=1; j<=m; j++)
+				for(int k=1; k<=m; k++)
 					cmax(f[i][j][k], f[i][j][k]+f[i+f[i][j][k]][j][k]);
-				}
-			}
+		for(int i=1; i<=m; i++)
+			for(int j=1; j<=n; j++)
+				for(int k=1; k<=n; k++)
+					cmax(g[i][j][k], g[i][j][k]+g[i+g[i][j][k]][j][k]);
+		for(int i=1; i<=m; i++)
+			for(int j=1; j<=n; j++)
+				for(int k=j; k<=n; k++)
+					cmax(f[j][i][i+g[i][j][k]-1], k-j+1);
+		if(f[1][1][m] == n)
+		{
+			printf("%d\n", r);
+			return;
 		}
-		
 	}
+	printf("16\n");
 }
 
 int main()
